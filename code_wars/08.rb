77db2a461bf0418aeac179fc008dce2ee53453ef
Juliette Sinibardy <juju.siny@gmail.com>
2019-12-22 10:29:11 +0100

    :ok_hand: Add some test cases to codewars exercise

--- code_wars/08.rb
+++ code_wars/08.rb
@@ -11,17 +11,12 @@
 LETTERS = ('a'..'z').to_a
 
 def alphabet_position(text)
-  numbers = []
-
-  text.chars.map(&:downcase).each do |char|
-    if LETTERS.include?(char)
-      numbers << LETTERS.index(char) + 1
-    end
-  end
-
-  numbers.join(' ')
+  text.chars.map(&:downcase).each_with_object([]) do |char, numbers|
+    numbers << LETTERS.index(char) + 1 if LETTERS.include?(char)
+  end.join(' ')
 end
 
+p alphabet_position("Hello") == "8 5 12 12 15"
 p alphabet_position("The sunset sets at twelve o' clock.") == "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
 p alphabet_position("-.-'") == ""
 
