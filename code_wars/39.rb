e5915ad480a0b4e71bcce34e9c4ed3895da9c293
Juliette Sinibardy <juju.siny@gmail.com>
2020-01-02 11:01:38 +0100

    :sparkles: Solve two unfinished katas

--- code_wars/39.rb
+++ code_wars/39.rb
@@ -1,11 +1,26 @@
 # https://www.codewars.com/kata/55c04b4cc56a697bb0000048
 
 def scramble(str1, str2)
-  (str2.chars - str1.chars).empty?
+  chars = str1.chars
+
+  str2.chars.each do |n|
+    return false unless chars.index(n)
+    chars.delete_at(chars.index(n))
+  end
+  true
 end
 
-p scramble('rkqodlw', 'world') == true
+p scramble('rkqodlw', 'world')              == true
 p scramble('cedewaraaossoqqyt', 'codewars') == true
-p scramble('katas', 'steak') == false
+p scramble('katas', 'steak')                == false
+p scramble('scriptjavx', 'javascript')      == false
+p scramble('javscripts', 'javascript')      == false
 
-# Can't submit it - Issue with the test cases?
+# Other ingenious solutions
+def scramble(s1, s2)
+  s2.chars.uniq.all? { |x| s2.count(x)<=s1.count(x) }
+end
+
+def scramble(s1,s2)
+  ('a'..'z').all? { |c| s1.count(c) >= s2.count(c) }  
+end
--- code_wars/41.rb
+++ code_wars/41.rb
@@ -1,22 +1,35 @@
 # https://www.codewars.com/kata/string-incrementer/train/ruby
 
-def increment_string(str)
-  base_str = str.split(/[0-9]/).join
-  num = extract_num(str)
-  base_str + num.next
+def increment_string(s)
+  return s + '1' unless s[-1] =~ /\d+/
+  s.sub(/\d*$/, s.scan(/\d+$/).join.next)
 end
 
-def extract_num(str)
-  num = str.chars.select { |i| i =~ /[0-9]/ }
-  num.empty? ? '0' : num.join
-end
+# First line: when the str doesn't end with a number
+# Second line: replace last digits by the next digit
 
-
-p increment_string("foo") == "foo1"
+p increment_string("foo")       == "foo1"
 p increment_string("foobar001") == "foobar002"
-p increment_string("foobar1") == "foobar2"
-p increment_string("foobar00") == "foobar01"
-p increment_string("foobar99") == "foobar100"
-p increment_string("") == "1"
-p increment_string("f00bar") #== "f00bar1"
-p increment_string("f00b4r") #== "f00b4r1"
+p increment_string("foobar1")   == "foobar2"
+p increment_string("foobar00")  == "foobar01"
+p increment_string("foobar99")  == "foobar100"
+p increment_string("")          == "1"
+p increment_string("f00bar")    == "f00bar1"
+p increment_string("f00b4r")    == "f00b4r1"
+p increment_string("f00bar3")   == "f00bar4"
+
+# Clever kata
+
+def increment_string(input)
+  input.sub(/\d*$/) { |n| n.empty? ? 1 : n.succ }
+end
+
+p increment_string("foo")       == "foo1"
+p increment_string("foobar001") == "foobar002"
+p increment_string("foobar1")   == "foobar2"
+p increment_string("foobar00")  == "foobar01"
+p increment_string("foobar99")  == "foobar100"
+p increment_string("")          == "1"
+p increment_string("f00bar")    == "f00bar1"
+p increment_string("f00b4r")    == "f00b4r1"
+p increment_string("f00bar3")   == "f00bar4"
