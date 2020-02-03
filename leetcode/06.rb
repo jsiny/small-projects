716384954cfb788026617291c28d601503297022
Juliette Sinibardy <juju.siny@gmail.com>
2020-02-03 11:00:46 +0100

    :ok_hand: Enhance LC #6

--- leetcode/06.rb
+++ leetcode/06.rb
@@ -1,9 +1,11 @@
 # https://leetcode.com/problems/zigzag-conversion/
 
+# First (passing) algorithm
+
 def convert(s, num_rows)
   return s if num_rows == 1
 
-  res  = Array.new(num_rows) { Array.new }
+  res  = Array.new(num_rows) { [] }
   i    = 0    # top-to-bottom-to-top index
   j    = 0    # left-to-right index
   down = true
@@ -30,6 +32,28 @@ def convert(s, num_rows)
 
   res.map(&:compact).flatten.join
 end
+
+# Enhanced solution
+
+def convert(s, num_rows)
+  return s if num_rows == 1
+
+  zigzag = Array.new(num_rows) { '' }
+  row    = 0
+  down   = true
+
+  s.each_char do |char|
+    down = false if row == num_rows - 1
+    down = true  if row.zero?
+    
+    zigzag[row] += char
+
+    row = (down ? row + 1 : row - 1)
+  end
+
+  zigzag.join
+end
+
 
 p convert("ABC", 1) == "ABC"
 
